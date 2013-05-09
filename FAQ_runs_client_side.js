6869f7127d5b9b5d527f117332186204537dd01b
Adal Bermann <adal@adaldesign.com>
2013-05-08 18:06:38 -0700

    Updated code to include search, tag and scroll to functionality

--- FAQ_runs_client_side.js
+++ FAQ_runs_client_side.js
@@ -5,12 +5,40 @@ $('#FAQ section h2, #FAQ section h3').each(function( index, domEle ) {
   
   var $class = '';
 	if ( $(domEle).is('h3') ) { $class = 'class="space-left"'; }
+	
   $('#FAQ .side-nav').append('<li><a points="h-' + index + '"' + $class + '">' + $(domEle).clone().text() + '</a></li>');
+  
   $(this).attr('id', 'h-' + index );
 });
 
 $('#FAQ .side-nav a').click( function() {
+	
   nameTag = $('#' + $(this).attr('points') );
   $('html,body').animate({scrollTop: nameTag.offset().top - 100},'slow');
   return false;
+  
 });
+
+
+// Then also bind a search form to a smooth search, tag and scroll to function
+
+jQuery.fn.highlight = function (str, className) {
+  if ( str != '' ) {
+	$('.' + className).removeClass(className);
+	var regex = new RegExp(str, "gi");
+	this.each(function () {
+		$(this).contents().filter(function() {
+			return this.nodeType == 3 && regex.test(this.nodeValue);
+		}).replaceWith(function() {
+			return (this.nodeValue || "").replace(regex, function(match) {
+				return "<span class=\"" + className + "\">" + match + "</span>";
+			});
+		});
+	});
+	return $('html,body').animate({scrollTop: $('.' + className + ':first').offset().top - 100},'slow');
+  }
+};
+
+// Edit these #search-button and #FAQ-search to match your HTML
+$('#search-button').click( function() { $("#FAQ section *").highlight($( '#FAQ-search').val(), "label"); });
+$('#FAQ-search').keypress( function(e) { if(e.keyCode == 13) { $("#FAQ section *").highlight($( '#FAQ-search').val(), "label"); } } );
