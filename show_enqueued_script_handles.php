a6914d05040d8eae95178caa97b9075b8be35f85
Adal Bermann <adal@adaldesign.com>
2013-06-03 14:47:13 -0700

    also show styles

--- show_enqueued_script_handles.php
+++ show_enqueued_script_handles.php
@@ -2,9 +2,12 @@
 
 function wpa54064_inspect_scripts() {
     global $wp_scripts;
-    foreach( $wp_scripts->queue as $handle ) :
-        echo $handle . ' | ';
-    endforeach;
+	echo 'Scripts: ';
+    foreach( $wp_scripts->queue as $handle ) : echo $handle . ' | '; endforeach;
+	
+	global $wp_styles;
+	echo 'Styles: ';
+    foreach( $wp_styles->queue as $handle ) : echo $handle . ' | '; endforeach;
 }
 add_action( 'wp_print_scripts', 'wpa54064_inspect_scripts' );
 
